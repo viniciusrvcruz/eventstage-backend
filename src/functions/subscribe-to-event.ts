dc1708aa53e2614638e0a7cf3c89e97bc6961ce3
vinicius_cruz_es <vinicius.cruz@enterscience.com.br>
2025-02-19 16:28:00 -0300

    feat: get subscriber invites count

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -1,15 +1,18 @@
 import { eq } from "drizzle-orm"
 import { db } from "../drizzle/client"
 import { subscriptions } from "../drizzle/schema/subscriptions"
+import { redis } from "../redis/client"
 
 interface SubscribeToEventParams {
   name: string
   email: string
+  referrerId?: string | null
 }
 
 export async function subscribeToEvent({
   name,
   email,
+  referrerId
 }: SubscribeToEventParams) {
   const subscribers = await db
     .select()
@@ -24,6 +27,10 @@ export async function subscribeToEvent({
     name,
     email,
   }).returning()
+
+  if(referrerId) {
+    await redis.zincrby('referral:ranking', 1, referrerId)
+  }
 
   const subscriber = result[0]
 
--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ import { env } from './env'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 import { accessInviteLinkRoute } from './routes/access-invite-link-route'
 import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route'
+import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -37,6 +38,7 @@ app.register(fastifySwaggerUi, {
 app.register(subscribeToEventRoute)
 app.register(accessInviteLinkRoute)
 app.register(getSubscriberInviteClicksRoute)
+app.register(getSubscriberInvitesCountRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running!')
